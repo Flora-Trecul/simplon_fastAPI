c55ff1d7e5a184301a639cca499e1f9e754f97b7
Flora T <flora.trecul@gmail.com>
2026-02-25 11:57:46 +0100

    Feat: update requirements and add database creation to sys argv

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,13 @@
-
+import sys
 from sqlalchemy import create_engine
 from app.models.models import Base
 
-engine= create_engine("sqlite:///data/simplon.db")
-Base.metadata.create_all(engine)
+
+# On crée la database avec la commande 'python -m app.core.database --create-db' dans le terminal depuis la racine
+if __name__ == "__main__":
+    if "--create-db" in sys.argv:
+
+        db_url = "sqlite:///app/data/simplon.db"
+        engine = create_engine(db_url)
+
+        Base.metadata.create_all(engine)
