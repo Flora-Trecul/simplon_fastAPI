f3ec89a16bd0d24e4b3e957148a202d18e9938c5
edilene <eassuncaosilva@gmail.com>
2026-02-26 16:53:47 +0100

    Feat: Inscription rules implemented

--- app/crud/courses.py
+++ app/crud/courses.py
@@ -3,11 +3,11 @@ from app.schemas.courses import CourseCreate, CourseUpdate
 from app.models.models import Course
 
 def create_course(db:Session, course: CourseCreate):
-    course = Course(**course.model_dump())
-    db.add(course)
+    db_course = Course(**course.model_dump())
+    db.add(db_course)
     db.commit()
-    db.refresh(course)
-    return course
+    db.refresh(db_course)
+    return db_course
 
 def get_course(db: Session, courses_id: int):
     return db.query(Course).filter(Course.id == courses_id).first()
@@ -16,21 +16,22 @@ def get_all_courses(db: Session, skip: int =0, limit: int =100):
     return db.query(Course).offset(skip).limit(limit).all()
 
 def update_course(db: Session, courses_id: int, course: CourseUpdate):
-    course = get_course(db, courses_id)
-    if not course:
+    db_course = get_course(db, courses_id)
+    if not db_course:
         return None
     
     for field, value in course.model_dump(exclude_unset=True).items():
-        setattr(course, field, value)
+        setattr(db_course, field, value)
     
     db.commit()
-    db.refresh(course)
-    return course
+    db.refresh(db_course)
+    return db_course
 
 def delete_course(db: Session, courses_id:int):
-    course = get_course(db, courses_id)
-    if course:
-        db.delete(course)
-        db.commit()
-    return course
+    db_course = get_course(db, courses_id)
+    if not db_course:
+        return None
+    db.delete(db_course)
+    db.commit()
+    return db_course
         
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.routers import courses
+from app.api.routers import courses, inscriptions
 
 app = FastAPI(title="Simplon API")
 
 app.include_router(courses.router)
+app.include_router(inscriptions.router)
