facdc601623f1fa4fc7ab206af6a878f2940c3a4
Fatima <you@example.com>
2026-03-02 15:13:13 +0100

    feat: add condition to the inscription function

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -17,7 +17,7 @@ def read_users(db: Session = Depends(get_db)):
     users = get_all_users(db=db)
     return users
 
-@router.get("/id/{user_id}", response_model=UserResponse)
+@router.get("/{user_id}", response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = get_user(db=db, user_id=user_id)
     if not user:
@@ -49,21 +49,21 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email existe déjà")
     return crud_create_user(db=db, user=user)
 
-@router.patch("/id/{user_id}", response_model=UserResponse)
+@router.patch("/{user_id}", response_model=UserResponse)
 def update_user(user_id: int, user_data: UserUpdate, db: Session = Depends(get_db)):
     user = crud_update_user(db=db, user_id=user_id, user_data=user_data)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Utilisateur introuvable")
     return user
 
-@router.delete("/id/{user_id}")
+@router.delete("/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     user = crud_delete_user(db=db, user_id=user_id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Utilisateur introuvable")
     return {"message": "Utilisateur supprimé"}
 
-@router.delete("/id/{user_id}/session/{session_id}")
+@router.delete("/{user_id}/session/{session_id}")
 def delete_session(user_id: int, session_id: int, db: Session = Depends(get_db)):
     session = crud_delete_session(db=db, user_id=user_id, session_id=session_id)
     if not session:
@@ -71,7 +71,7 @@ def delete_session(user_id: int, session_id: int, db: Session = Depends(get_db))
     return {"message": "Session supprimé"}
 
 
-@router.post("/id/{user_id}/session/{session_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/{user_id}/session/{session_id}", status_code=status.HTTP_201_CREATED)
 def inscription(user_id: int, session_id: int, db: Session = Depends(get_db)):
     user = get_user(db, user_id)
     session = get_learning_session(db, session_id)
@@ -82,15 +82,16 @@ def inscription(user_id: int, session_id: int, db: Session = Depends(get_db)):
     if exist_inscription:
         raise HTTPException(status_code=400, detail="Utilisateur déjà inscrit")
     
-    current_count = count_inscriptions(db, session_id)
-    if current_count >= session.max_capacity:
-        raise HTTPException(status_code=400, detail="Capacité maximale atteinte pour cette session")
+    if user.role == "apprenant":
+        current_count = count_inscriptions(db, session_id)
+        if current_count >= session.max_capacity:
+            raise HTTPException(status_code=400, detail="Capacité maximale atteinte pour cette session")
 
     create_inscription(db, user_id, session_id)
     return {"message": "Utilisateur inscrit avec succès"}
 
 
-@router.get("/id/{user_id}/sessions", response_model=List[LSFull])
+@router.get("/{user_id}/sessions", response_model=List[LSFull])
 def get_sessions(user_id: int, db: Session = Depends(get_db)):
     sessions = get_all_sessions(db, user_id)
     return sessions
